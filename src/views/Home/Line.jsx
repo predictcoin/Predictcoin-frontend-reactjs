2a72a9a8335586e4a61fd39bbd04495730959071
Horlamide <olamide.jubril@taxtech.com.ng>
2021-12-12 00:36:33 +0100

    Updated chart data

--- src/views/Home/Line.jsx
+++ src/views/Home/Line.jsx
@@ -2,44 +2,16 @@ import React from "react";
 import { Chart } from "react-charts";
 
 const Line = ({ dataset }) => {
-  
-
-
-  const chunk = arr => {
-    const size = 2;
-    const chunkedArr = [];
-
-    for (let i = 0; i < arr.length; i++) {
-      const last = chunkedArr[chunkedArr.length - 1];
-      if (!last || last.length === size) {
-        chunkedArr.push([arr[i]]);
-      } else {
-        last.push(arr[i]);
-      }
-    }
-
-    return chunkedArr;
-  };
-
-  const indexOfDataset = dataset.map((__, index) => {
-    return index;
-  });
-
-  const resultDataSet = chunk(dataset);
-
-  const resultIndex = chunk(indexOfDataset);
-
-  console.log("RES: ", resultDataSet)
+  const resultDataSet = dataset.map((data, index) => [index, data]);
 
   const data = React.useMemo(
     () => [
       {
         label: "Series 1",
-        // data: [[0, 1], [1, 2], [2, 4], [3, 2], [4, 7]]
         data: resultDataSet
       },
     ],
-    [resultDataSet, resultIndex]
+    [resultDataSet]
   );
 
   const axes = React.useMemo(
