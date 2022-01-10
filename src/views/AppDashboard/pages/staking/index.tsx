3a03c5a403d90ea7cfde23a626d6eab14373284e
caspero-62 <nwaforuchechukwu2007@gmail.com>
2022-01-10 14:06:23 +0100

    staking pages initialization

---
+++ src/views/AppDashboard/pages/staking/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+import './staking.styles.scss';
+
+const Staking: FC = () => {
+    return (
+        <section id='staking'>
+            <p>hello there</p>
+        </section>
+    );
+};
+
+export default Staking;
